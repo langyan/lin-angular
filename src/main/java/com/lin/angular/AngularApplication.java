fb5314523cbfc78ba3b69e36c1add98ddc74ce0d
michaellin <langyan.lin@gmail.com>
2017-03-28 16:49:56 +0800

    添加目录

---
+++ src/main/java/com/lin/angular/AngularApplication.java
@@ -0,0 +1,57 @@
+package com.lin.angular;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AngularApplication {
+
+    // @RequestMapping("/user")
+    // public Map<String, String> user(Principal user) {
+    // return Collections.singletonMap("name", user.getName());
+    // }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularApplication.class, args);
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/").permitAll().anyRequest()
+                    .hasRole("USER");
+            // @formatter:on
+        }
+    }
+
+}
--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,24 +1,76 @@
-angular.module('hello', []).controller('home',
+angular.module('hello', ['ngRoute']).config(function($routeProvider, $httpProvider) {
 
-function($http) {
-	
+	$routeProvider.when('/', {
+		templateUrl : 'home.html',
+		controller : 'home',
+		controllerAs : 'controller'
+	}).when('/login', {
+		templateUrl : 'login.html',
+		controller : 'navigation',
+		controllerAs : 'controller'
+	}).otherwise('/');
+
+	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+}).controller('navigation', function($rootScope, $http, $location, $route) {
+
 	var self = this;
-	
-	console.log('Loading');
 
-	$http.get('user').then(function(response) {
-		var data = response.data;
-		if (data.name) {
-			self.authenticated = true;
-			self.user = data.name
-			$http.get('/resource/').then(function(response) {
-				self.greeting = response.data;
-			})
-		} else {
-			self.authenticated = false;
-		}
-	}, function() {
-		self.authenticated = false;
-	});
+	self.tab = function(route) {
+		return $route.current && route === $route.current.controller;
+	};
 
+	var authenticate = function(credentials, callback) {
+
+		var headers = credentials ? {
+			authorization : "Basic " + btoa(credentials.username + ":" + credentials.password)
+		} : {};
+
+		$http.get('user', {
+			headers : headers
+		}).then(function(response) {
+			if (response.data.name) {
+				$rootScope.authenticated = true;
+			} else {
+				$rootScope.authenticated = false;
+			}
+			callback && callback($rootScope.authenticated);
+		}, function() {
+			$rootScope.authenticated = false;
+			callback && callback(false);
+		});
+
+	}
+
+	authenticate();
+
+	self.credentials = {};
+	self.login = function() {
+		authenticate(self.credentials, function(authenticated) {
+			if (authenticated) {
+				console.log("Login succeeded")
+				$location.path("/");
+				self.error = false;
+				$rootScope.authenticated = true;
+			} else {
+				console.log("Login failed")
+				$location.path("/login");
+				self.error = true;
+				$rootScope.authenticated = false;
+			}
+		})
+	};
+
+	self.logout = function() {
+		$http.post('logout', {}).finally(function() {
+			$rootScope.authenticated = false;
+			$location.path("/");
+		});
+	}
+
+}).controller('home', function($http) {
+	var self = this;
+	$http.get('/resource/').then(function(response) {
+		self.greeting = response.data;
+	})
 });
