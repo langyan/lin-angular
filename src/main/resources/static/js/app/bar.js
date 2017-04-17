4de3e04403d62d3d807791d72fbe8270ef3e1dda
michael <michael@local-michael>
2017-04-17 21:39:58 +0800

    init

---
+++ src/main/resources/static/js/app/bar.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+
+module.exports = function() {
+  var greet = document.createElement('div');
+  greet.textContent = "4 Hi there and greetings!";
+  return greet;
+};
---
+++ src/main/resources/static/js/src/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+
+import bar from './bar';
+
+bar();
